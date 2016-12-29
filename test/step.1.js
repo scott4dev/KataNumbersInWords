5de039d651a64c18f78d5527113f9638f79827d6
Claudio Maccari <claudio@jitidea.net>
2016-12-29 19:23:35 +0100

    conver single digit

--- test/step.1.js
+++ test/step.1.js
@@ -23,4 +23,46 @@ describe('Converterl', () => {
 		result.should.equal('two');
 	});
 
+	it('should convert 3', () => {
+		const sut = new Converter();
+		const result = sut.convert(3);
+		result.should.equal('three');
+	});
+
+	it('should convert 4', () => {
+		const sut = new Converter();
+		const result = sut.convert(4);
+		result.should.equal('four');
+	});
+
+	it('should convert 5', () => {
+		const sut = new Converter();
+		const result = sut.convert(5);
+		result.should.equal('five');
+	});
+
+	it('should convert 6', () => {
+		const sut = new Converter();
+		const result = sut.convert(6);
+		result.should.equal('six');
+	});
+
+	it('should convert 7', () => {
+		const sut = new Converter();
+		const result = sut.convert(7);
+		result.should.equal('seven');
+	});
+
+	it('should convert 8', () => {
+		const sut = new Converter();
+		const result = sut.convert(8);
+		result.should.equal('eigth');
+	});
+
+	it('should convert 9', () => {
+		const sut = new Converter();
+		const result = sut.convert(9);
+		result.should.equal('nine');
+	});
+
 });
