af9f1094fa8e532e7057d2a9e1f75125ce942557
Claudio Maccari <claudio@jitidea.net>
2016-12-29 20:06:16 +0100

    fix test name

--- test/step.7.js
+++ test/step.7.js
@@ -5,19 +5,19 @@ const Converter = require('../src/index');
 
 describe('Convert from 50 to 59', () => {
 
-	it('should convert 30', () => {
+	it('should convert 50', () => {
 		const sut = new Converter();
 		const result = sut.convert(50);
 		result.should.equal('fifty');
 	});
 
-	it('should convert 42', () => {
+	it('should convert 52', () => {
 		const sut = new Converter();
 		const result = sut.convert(52);
 		result.should.equal('fifty-two');
 	});
 	
-	it('should convert 39', () => {
+	it('should convert 58', () => {
 		const sut = new Converter();
 		const result = sut.convert(58);
 		result.should.equal('fifty-eight');
