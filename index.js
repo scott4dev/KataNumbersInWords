2d4d6dab155122748c73f0301a6d81c62dddeaab
Claudio Maccari <claudio@jitidea.net>
2016-12-29 19:13:59 +0100

    convert only 1

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const Converter = class Converter {
+	convert() {
+		return 'one';
+	}
+}
+
+module.exports = Converter;
---
+++ test/step.1.js
@@ -0,0 +1,14 @@
+const chai = require('chai')
+	, should = chai.should();
+
+const Converter = require('../index');
+
+describe('Converterl', () => {
+
+	it('should convert 1', () => {
+		const sut = new Converter();
+		const result = sut.convert(1);
+		result.should.equal('one');
+	});
+	
+});
