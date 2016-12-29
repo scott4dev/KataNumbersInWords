2187ec2232bd663eb27d0236cb7ee42898168287
Claudio Maccari <claudio@jitidea.net>
2016-12-29 19:45:23 +0100

    Convert from 20 to 29

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@
 const Converter = class Converter {
 
 	convert(number) {
-		const numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+		const numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
+
+		if (number > 20) {
+			const index = number - 20;
+			return `twenty-${numbers[index]}`;
+		}
+
 		return numbers[number];
 	}
 }
